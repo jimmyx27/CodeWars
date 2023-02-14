3b57113d26f8b14f8b62c05a9e61aa2372b00741
jimmy <jimmyx27@gmail.com>
2023-02-14 12:48:44 -0500

    solved makeString.js

---
+++ makeString.js
@@ -0,0 +1,9 @@
+function makeString(s) {
+  let arr = s.split(" ");
+  let newArr = [];
+  arr.forEach((element) => newArr.push(element[0]));
+  console.log(newArr.join(""));
+  return newArr.join("");
+}
+
+makeString("Hello this is a test");
